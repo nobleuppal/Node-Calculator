9621c8fe46e9f8282f3e58e4ba5b95e32198f35a
Noble Uppal <n13singhz@live.ca>
2021-12-09 00:41:14 -0500

    finished calculator

--- nodecalc.js
+++ nodecalc.js
@@ -1,4 +1,29 @@
 var rs = require('readline-sync');
-
-var operation = rs.question('What operation would you like to perform? ');
-console.log('You picked ' + operation);
+var operation;
+var number;
+var numberTwo;
+var answer;
+while (operation !='/' || operation != '*' || operation != '-' || operation != '+') {
+  operation = rs.question('What operation would you like to perform? ');
+  if (operation =='/' || operation == '*' || operation == '-' || operation == '+') {
+    number = rs.questionFloat('Please enter the first number: ', {limitMessage: 'This is not a number'});
+    numberTwo = rs.questionFloat('Please enter the second number: ', {limitMessage: 'This is not a number'});
+    if (operation == '/') {
+    answer = number / numberTwo;
+    }
+    else if (operation == '*') {
+      answer = number * numberTwo;
+    } 
+    else if (operation == '-') {
+      answer = number - numberTwo;
+    } 
+    else if (operation == '+') {
+      answer = number + numberTwo;
+    } 
+    console.log('The result is:', answer);
+    return 0;
+  }
+  else {
+    console.log('That is not a valid operation');
+  }
+}
